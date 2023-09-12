cbcf7ab62401a7864434a498c29df2838cb861a4
Dylan <61059252+DylanBruner@users.noreply.github.com>
2023-09-11 22:00:56 -0400

    Update buildtool

--- tools/clean.py
+++
@@ -1,7 +0,0 @@
-import os, shutil
-
-if os.path.exists('bin/dylanbruner'): shutil.rmtree('bin/dylanbruner')
-os.mkdir('bin/dylanbruner')
-if os.path.exists('C:/robocode/robots/dylanbruner'): shutil.rmtree('C:/robocode/robots/dylanbruner')
-os.mkdir('C:/robocode/robots/dylanbruner')
-print('[INFO] Cleaned bin/dylanbruner')
--- tools/compile.bat
+++
@@ -1,3 +0,0 @@
-@echo OFF
-REM javac -classpath libs/*; -d bin -sourcepath dylanbruner dylanbruner/Alphabet.java dylanbruner/*.java dylanbruner/data/*.java dylanbruner/gun/*.java dylanbruner/move/*.java dylanbruner/util/*.java dylanbruner/funnystuff/*.java
-"C:\Program Files\Java\jdk-9.0.4\bin\javac.exe" -classpath libs/*; -d bin -sourcepath dylanbruner dylanbruner/Alphabet.java dylanbruner/*.java dylanbruner/data/*.java dylanbruner/gun/*.java dylanbruner/move/*.java dylanbruner/util/*.java dylanbruner/funnystuff/*.java dylanbruner/special/*.java dylanbruner/testing/*.java
